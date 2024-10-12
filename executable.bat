6e74039a233091ca65216cbd4a6ece90430811b8
Mateus Silva <mateus.silva8044@gmail.com>
2024-10-12 09:22:34 -0300

    atualizando

--- executable.bat
+++ executable.bat
@@ -1,10 +1,10 @@
 pyinstaller --onefile --noconsole --icon "panico.ico" --add-data "panico.ico;." --name main main.py
 move dist\main.exe executables
 cd update
-pyinstaller --onefile --icon "panico.ico" --add-data "version.txt;." --name update update.py
+pyinstaller --onefile --icon "..\panico.ico" --add-data "version.txt;." --name update update.py
 cd ..
 move update\dist\update.exe executables
 cd installer
-pyinstaller --onefile --icon "panico.ico" --name installer installer.py
+pyinstaller --onefile --icon "..\panico.ico" --name installer installer.py
 cd ..
 move installer\dist\installer.exe executables
