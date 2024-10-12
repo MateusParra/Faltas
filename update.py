58cade990787aec2d30d4681d35b6d70528b6050
Mateus Silva <mateus.silva8044@gmail.com>
2024-10-12 02:11:53 -0300

    sla

--- update.py
+++ update.py
@@ -4,3 +4,7 @@ import os
 class Update:
     def __init__(self):
         path = os.path.dirname(__file__)
+
+    def check_version(self):
+        response = requests.get("https://github.com/MateusParra/Faltas/raw/refs/heads/desenvolvimento/dist/version.txt")
+        new_version = response.text
