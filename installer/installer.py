135c05f087c875bc4563219c191f54412641a9d1
Mateus Silva <mateus.silva8044@gmail.com>
2024-10-15 14:27:16 -0300

    atualizando

--- installer/installer.py
+++ installer/installer.py
@@ -1,19 +1,35 @@
 import os
 import requests
+import tkinter as tk
+import sys
 
 class Installer:
     def __init__(self, path):
         self.path = path
 
-    def install_file(self, urls):
+    def error(self, text):
+        if isinstance(text, str):
+            root = tk.Tk()
+            root.geometry('400x200')
+            root.title('Error')
+            root.iconbitmap(os.path.join(os.path.dirname(__file__), 'icon.ico'))
+            frame_geral = tk.Frame(root)
+            frame_geral.pack()
+            title_error = tk.Label(frame_geral, text=text)
+            title_error.pack(pady=10)
+
+            root.wait_window(root)
+            sys.exit()
+
+    def install_file(self, url):
         if os.path.exists(self.path):
             os.system(f'del /f /q "{os.path.join(self.path, '*')}"')
             os.system(f'rmdir /s /q "{self.path}"')
         os.makedirs(self.path, exist_ok=False)  # Cria a pasta, o exist_ok serve para caso ela exista ele não de erro no terminal
         # se for True
 
-        urls = list(urls)
-        for url in urls:
+        links = list(url)
+        for url in links:
             name = url
             while True:
                 search = name.find('/')
@@ -24,15 +40,17 @@ class Installer:
 
             file_name = name
             full_path = os.path.join(self.path, file_name)  # junta o caminho com o nome do arquivo
+            try:
+                response = requests.get(url)
 
-            response = requests.get(url)
-
-            if response.status_code == 200:
-                with open(full_path, "wb") as file:  # wb é formato de escrita em binario, 'w' write, 'b' binarie
-                    file.write(response.content)  # o codigo é pego em formato binario por ser um executavel
-                print(f'{file_name} downloaded in {path}')
-            else:
-                print(f'failed to download, error code: {response.status_code}')
+                if response.status_code == 200:
+                    with open(full_path, "wb") as file:  # wb é formato de escrita em binario, 'w' write, 'b' binarie
+                        file.write(response.content)  # o codigo é pego em formato binario por ser um executavel
+                    print(f'{file_name} downloaded in {path}')
+                else:
+                    self.error(f'Falha no download, codigo do erro: {response.status_code}')
+            except:
+                self.error('Falha no download, verifique sua conexão e tente novamente!')
 
     def shortcut(self, file_name, shortcut_name):
         files = os.listdir(self.path)
@@ -52,11 +70,11 @@ class Installer:
         desktop = '%USERPROFILE%\\Desktop'
         full_path = os.path.join(path, shortcut_name)
         os.system(f'move  "{full_path}" "{desktop}"')
-        print(f'{shortcut_name} moved to {start_menu} and {desktop}')
+        print(f'{shortcut_name} moved to {desktop}')
 
 
 if __name__ == '__main__':
-    path = 'C:\\Program Files (x86)\\faltas'
+    path = 'C:\\faltas'
     url1 = 'https://github.com/MateusParra/Faltas/raw/refs/heads/main/executables/main.exe'
     url2 = 'https://github.com/MateusParra/Faltas/raw/refs/heads/main/executables/update.exe'
     url3 = 'https://github.com/MateusParra/Faltas/raw/refs/heads/main/version.txt'
--- update/update.py
+++ update/update.py
@@ -1,9 +1,23 @@
 import requests
 import os
+import tkinter as tk
 
 class Update:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self):
+        self.path = os.path.dirname(__file__)
+
+    def error(self, text):
+        if isinstance(text, str):
+            root = tk.Tk()
+            root.geometry('400x200')
+            root.title('Error')
+            root.iconbitmap(os.path.join(os.path.dirname(__file__), 'icon.ico'))
+            frame_geral = tk.Frame(root)
+            frame_geral.pack()
+            title_error = tk.Label(frame_geral, text=text)
+            title_error.pack(pady=10)
+
+            root.wait_window(root)
 
     def check_version(self):
         version = []
@@ -16,14 +30,20 @@ class Update:
         else:
             version = version[0].strip()
 
-        response = requests.get("https://github.com/MateusParra/Faltas/raw/refs/heads/main/version.txt")
+        try:
+            response = requests.get("https://github.com/MateusParra/Faltas/raw/refs/heads/main/version.txt")
 
-        if response.status_code == 200:
-            new_version = response.text
-        else:
-            raise ValueError(f"Não foi possivel acessar! -- Codigo: {response.status_code}")
+            if response.status_code == 200:
+                new_version = response.text
+            else:
+                new_version = 0
+        except:
+            new_version = 0
+            status_code = 0
+            self.error('Falha no download, verifique sua conexão e tente novamente!')
+            return version == new_version, status_code
 
-        return version == new_version
+        return version == new_version, response.status_code
 
     def update(self, url):
         file_name = url
@@ -34,20 +54,25 @@ class Update:
             else:
                 break
 
-        updated = self.check_version()
+        updated, status_code = self.check_version()
 
-        if not updated:
+        try:
             response = requests.get(url)
-            if response.status_code == 200:
-                with open(os.path.join(self.path, file_name), "wb") as file:
-                    file.write(response.content)
+            if not updated:
+                if response.status_code == 200 and status_code == 200:
+                    with open(os.path.join(self.path, file_name), "wb") as file:
+                        file.write(response.content)
 
-                with open(os.path.join(self.path, 'version.txt'), 'w') as file:
-                    new_version = requests.get("https://github.com/MateusParra/Faltas/raw/refs/heads/main/version.txt")
-                    new_version = new_version.text
-                    file.write(new_version)
-            else:
-                print('Falha ao baixar o arquivo!')
+                    with open(os.path.join(self.path, 'version.txt'), 'w') as file:
+                        new_version = requests.get("https://github.com/MateusParra/Faltas/raw/refs/heads/main/version.txt")
+                        new_version = new_version.text
+                        file.write(new_version)
+                else:
+                    self.error(f'Falha ao instalar a atualização, codigo do erro: {status_code if status_code != 200 else response.status_code}')
+        except:
+            return
+
+
 
     def start_file(self, file_name):
         os.startfile(os.path.join(self.path, file_name))
@@ -56,8 +81,7 @@ class Update:
 
 if __name__ == '__main__':
     url = 'https://github.com/MateusParra/Faltas/raw/refs/heads/main/executables/main.exe'
-    path = 'C:\\Program Files (x86)\\faltas'
 
-    update = Update(path)
+    update = Update()
     update.update(url)
     update.start_file('main.exe')
