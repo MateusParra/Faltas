144b113e46377740c7cadfcaca2cd157a271cc05
Mateus Silva <mateus.silva8044@gmail.com>
2024-10-15 22:45:44 -0300

    atualizando

--- update/update.bat
+++ update/update.bat
@@ -1,6 +1,6 @@
 rmdir /s /q dist
 rmdir /s /q build
-pyinstaller --onefile --icon "panico.ico" --add-data "panico.ico;." --name update update.py
+pyinstaller --onefile --noconsole --icon "panico.ico" --add-data "panico.ico;." --name update update.py
 cd ..
 move update\dist\update.exe executables
 exit
